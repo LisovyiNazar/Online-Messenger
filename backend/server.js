7ebf2e39d7c116c27bf184114ae53c3fcc5ccb36
na3alis <na3alis@gmail.com>
2022-04-16 20:28:23 +0300

    eslint-config

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 
-const PORT = process.env.PORT || 4000
+const app = express();
 
-app.get('/', (req,res) => {
-    res.send('ok')
-})
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('ok');
+});
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
+  console.log(`server is running on port ${PORT}`);
+});
